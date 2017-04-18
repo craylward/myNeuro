7d9f6872e0b0d23c71258b1186e7c3a8bd92d864
craylward <craylward@gmail.com>
2017-04-18 14:54:51 -0700

    Change file structure

---
+++ myNeuro/AlertOverlay.swift
@@ -0,0 +1,9 @@
+//
+//  UploadIndicator.swift
+//  myNeuro
+//
+//  Created by Charlie Aylward on 3/17/17.
+//  Copyright © 2017 SJM. All rights reserved.
+//
+
+import Foundation
---
+++ myNeuro/Results/ProcessTremor.swift
@@ -0,0 +1,9 @@
+//
+//  ProcessTremor.swift
+//  myNeuro
+//
+//  Created by Charlie Aylward on 3/16/17.
+//  Copyright © 2017 SJM. All rights reserved.
+//
+
+import Foundation
